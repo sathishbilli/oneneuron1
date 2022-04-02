cb31dd259799858d0f1badf8ad4a2425d3f7ef04
sathishbilli <sathishmahi398@gmail.com>
2022-04-02 18:51:38 +0530

    logging string

--- new/demo1.py
+++ new/demo1.py
@@ -10,7 +10,13 @@ def demo(df):
 
     Returns: 
         int
-        
+        demou
     """
-
+import logging
+logging_str="[%(asctime)s:%(levelname)s:%(module)s]%(message)"
+logging.basicConfig(level=logging.INFO,format=logging_str)
+def print_name(name):
+    logging.info(f'name is {name}')
+def add_no(n1,n2):
+    logging.info(f'final ans is {n1}+{n2}')
    
