324e87fd38d1a7e126e8e3b802a17881593ee555
sathishbilli <sathishmahi398@gmail.com>
2022-04-02 19:44:01 +0530

    make dir push in dir

--- demo2.py
+++ demo2.py
@@ -15,7 +15,7 @@ def demo(df):
 import logging
 import os
 logging_dir="log"
-os.mkdir(logging_dir,exists=True)
+os.makedirs(logging_dir,exists=True)
 logging_str="[%(asctime)s:%(levelname)s:%(module)s] %(message)s"
 logging.basicConfig(filename=os.path.join(logging_dir,"runnig_logs.log"),level=logging.INFO,format=logging_str)
 print()
