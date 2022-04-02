ce65dfcb1fb445d7415c756e31af81676f53ea74
sathishbilli <sathishmahi398@gmail.com>
2022-04-02 19:41:28 +0530

    make dir push in dir

--- new/demo1.py
+++ new/demo1.py
@@ -17,10 +17,15 @@ logging_str="[%(asctime)s:%(levelname)s:%(module)s] %(message)s"
 logging.basicConfig(level=logging.INFO,format=logging_str)
 print()
 logging.info("this a print name function")
-class demo:
+
 
     # def print_name(name):
     #     logging.info(f'name is {name}')
+try:
+    logging.info("________________________ strarting fun  ______________________________")
     def add_no(n1,n2):
-        logging.info('ans is ',int(n1)+int(n2))
+        logging.info('ans is ',int(n1)/int(n2))
+    logging.info("________________________ stop fun  ______________________________")
+except Exception as e:
+    logging.exception(e)
    
